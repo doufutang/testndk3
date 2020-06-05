4490b4b5efca550413c64ef740bb61b7c41fdf7d
doufutang <jiangyy0202@163.com>
2020-06-05 10:43:32 +0800

    just for test 2

--- app/src/main/java/com/bn/testndk3/MainActivity.java
+++ app/src/main/java/com/bn/testndk3/MainActivity.java
@@ -2,10 +2,12 @@ package com.bn.testndk3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends android.app.NativeActivity
 {
+    Log.d("main","For test");
     static
     {
         System.loadLibrary("bn-vulkan-lib");
